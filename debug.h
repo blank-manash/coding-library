907632f32cb34026d5302b1b3b50a3e9fbeb8922
Manash Baul <42531354+soranaimanas@users.noreply.github.com>
2020-06-08 00:49:27 +0530

    Add files via upload

---
+++ debug.h
@@ -0,0 +1,37 @@
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define deb(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+void debug_out() {
+    cerr << "-----------------\n";
+}
+template<typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    debug() << H;
+    debug_out(T...);
+}
+#define db(...) cerr << "[" << #__VA_ARGS__ << "]:\n", debug_out(__VA_ARGS__)
